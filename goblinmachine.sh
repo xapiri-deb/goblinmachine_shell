387c198bb91ca98c94c2def12f7e0da0b9bab189
xapiri-deb <momonomuni@gmail.com>
2024-07-09 21:03:23 -0300

    echo

---
+++ goblinmachine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " Calculadora de consumíveis Goblin Machine! "
+echo " Escolha uma opção: "
+echo " show - Mostrar lista de itens"
+echo " list - Fazer lista de materiais"
+echo " exit - Sair"
